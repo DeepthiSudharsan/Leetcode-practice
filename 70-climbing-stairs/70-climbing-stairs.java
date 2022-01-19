0d237f7b635a3fff217e43183878384c24587cfb
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2022-01-19 21:56:40 +0530

    Time: 0 ms (100.00%), Space: 37.8 MB (9.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,28 +1,44 @@
 class Solution {
-    HashMap<Integer,Integer> steps = new HashMap<Integer,Integer>();
+    // HashMap<Integer,Integer> steps = new HashMap<Integer,Integer>();
     public int climbStairs(int n) 
     {
-        int step_count = 0;
-        if(n == 1)
+    //     int step_count = 0;
+    //     if(n == 1)
+    //     {
+    //         return 1;
+    //     }
+    //     else if(n == 2)
+    //     {
+    //         return 2;
+    //     }
+    //     else
+    //     {
+    //         if(steps.containsKey(n))
+    //         {
+    //             step_count = steps.get(n);
+    //         }
+    //         else
+    //         {
+    //             step_count = climbStairs(n-1) + climbStairs(n-2);
+    //             steps.put(n,step_count);
+    //         }
+    //     }
+    //     return step_count;
+        int ways[] = new int[n+1];
+
+        ways[1] = 1;
+        if(n > 1)
         {
-            return 1;
-        }
-        else if(n == 2)
-        {
-            return 2;
-        }
-        else
-        {
-            if(steps.containsKey(n))
+            ways[2] = 2;
+
+            for(int i = 3 ; i < n+1 ; i++) 
             {
-                step_count = steps.get(n);
+                ways[i] = ways[i-1] + ways[i-2];
             }
-            else
-            {
-                step_count = climbStairs(n-1) + climbStairs(n-2);
-                steps.put(n,step_count);
-            }
+
         }
-        return step_count;
+        
+        return ways[n];
+    
     }
 }
