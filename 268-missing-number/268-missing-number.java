622571412686043c15615f4de7b11b39d98c73a8
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2022-02-10 20:24:50 +0530

    Time: 1 ms (73.81%), Space: 50.9 MB (20.68%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,27 +1,33 @@
 class Solution {
     public int missingNumber(int[] nums) 
     {
-        int flag = 0; 
-        for(int i = 0; i <= nums.length ; i++)
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++)
         {
-            for(int j = 0; j < nums.length;j++)
-            {
-                if(i == nums[j])
-                {
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0)
-            {
-                flag = i;
-                break;
-            }
-            else
-            {
-                flag = 0;
-            }
+            sum = sum + nums[i];
         }
-        return flag;
+        return (nums.length * (nums.length + 1)/2) - sum;
+    //     int flag = 0; 
+    //     for(int i = 0; i <= nums.length ; i++)
+    //     {
+    //         for(int j = 0; j < nums.length;j++)
+    //         {
+    //             if(i == nums[j])
+    //             {
+    //                 flag = 1;
+    //                 break;
+    //             }
+    //         }
+    //         if(flag == 0)
+    //         {
+    //             flag = i;
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             flag = 0;
+    //         }
+    //     }
+    //     return flag;
     }
 }
