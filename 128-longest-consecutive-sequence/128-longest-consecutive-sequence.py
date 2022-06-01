517a9fac2353f00476387534e5e9b4d5d60b29cd
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2022-06-01 12:24:03 +0530

    Time: 626 ms (43.65%), Space: 28 MB (39.83%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,36 +1,33 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
+#         numSet = set(nums)
+#         longest = 0
         
-        for n in nums:
-            # check if its the start of a sequence
-            if (n - 1) not in numSet:
-                length = 1
-                while (n + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
-        
-                
-                
-            
-        # maxlen = 0
-        # # nums.sort()
-        # temp = 0
-        # if len(nums) == 0:
-        #     return 0
-        # nos = sorted(list(set(nums)))
-        # for i in range(len(nos)-1):
-        #     if nos[i] == nos[i+1] - 1:
-        #         temp +=1
-        #     else:
-        #         if maxlen <= temp:
-        #             maxlen = temp
-        #         temp = 0
-        # if maxlen <= temp:
-        #     maxlen = temp
-        # return maxlen + 1
+#         for n in nums:
+#             # check if its the start of a sequence
+#             if (n - 1) not in numSet:
+#                 length = 1
+#                 while (n + length) in numSet:
+#                     length += 1
+#                 longest = max(length, longest)
+#         return longest
+         
+        maxlen = 0
+        # nums.sort()
+        temp = 0
+        if len(nums) == 0:
+            return 0
+        nos = sorted(list(set(nums)))
+        for i in range(len(nos)-1):
+            if nos[i] == nos[i+1] - 1:
+                temp +=1
+            else:
+                if maxlen <= temp:
+                    maxlen = temp
+                temp = 0
+        if maxlen <= temp:
+            maxlen = temp
+        return maxlen + 1
             
             
         
