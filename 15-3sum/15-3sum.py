8338f9946a9a2c021592f64992f5754c70f395b5
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2022-07-05 12:05:58 +0530

    Time: 6616 ms (8.73%), Space: 18.1 MB (72.37%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,16 +5,12 @@ class Solution:
         if len(nums) == 3:
             if sum(nums) == 0:
                 return [nums]
-            return []
-        # if len(nums) >= 4 and len(set(nums)) == 1 and nums[0] == 0:
-        #     return [[0,0,0]]            
+            return []           
         nums.sort()
         res = []
         for a in range(len(nums)-1):
             if a != 0 and nums[a] == nums[a-1]:
-                # print(a)
                 a += 1
-                # print(a)
             else:
                 b = a+1
                 c = len(nums)-1
@@ -27,10 +23,5 @@ class Solution:
                         c -=1
                     elif nums[b]+nums[c] < -nums[a]:
                         b +=1      
-        # for i in range(len(nums)-2):
-        #     for j in range(i+2,len(nums)):
-        #         triplet = 0 - (nums[i] + nums[j])
-        #         if triplet in nums[i+1:j] and ([nums[i],triplet,nums[j]] not in res):
-        #             res.append([nums[i],triplet,nums[j]])
         return res
                 
