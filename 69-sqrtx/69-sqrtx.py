97fcb9a635aff1106becc834c4418a3b1845fec8
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2022-07-25 10:21:41 +0530

    Time: 4433 ms (5.32%), Space: 13.9 MB (56.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,10 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         for i in range(x//2 + 2):
-            # if x == 0:
-            #     return 0
-            # if x == 1:
-                # return 1
             if i * i == x:
                 return i
             if i * i > x:
